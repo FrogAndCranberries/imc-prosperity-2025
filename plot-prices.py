1d53ff66fbff71397c1f0c0e0f4fa543ae0e86fc
FrogAndCranberries <strakmi@gmail.com>
2025-04-07 20:44:45 +0100

    Update requirements, finish basic plotting

--- plot-prices.py
+++ plot-prices.py
@@ -1,15 +1,20 @@
-from matplotlib import pyplot
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
 DAY = 0
 
-PRODUCT = "kelp"
+PRODUCT = "KELP"
 
 csv_path = f"./round-1-island-data-bottle/prices_round_1_day_{DAY}.csv"
 
-df = pd.read_csv(csv_path)
+df = pd.read_csv(csv_path, delimiter=';')
 
 print(df.head())
+print(df.keys())
+midprice = df[(df["product"] == PRODUCT)]["mid_price"]
+print(len(midprice))
 
-df["product"] == PRODUCT
+midprice.plot()
+plt.savefig('plots/plot.png')
+plt.show()
