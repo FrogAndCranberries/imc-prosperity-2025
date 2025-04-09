cc457abd5f7c7ad3c25bf7c93759d6268de1adee
FrogAndCranberries <strakmi@gmail.com>
2025-04-09 18:07:31 +0100

    smooth out plotting and improve plot savefile naming

--- plot-prices.py
+++ plot-prices.py
@@ -2,19 +2,25 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
+# Variables
 DAY = 0
-
 PRODUCT = "KELP"
+SMOOTHING_PERIOD = 100
+SMOOTHING_SIGMA = 20
 
+# Load dataframe
 csv_path = f"./round-1-island-data-bottle/prices_round_1_day_{DAY}.csv"
 
 df = pd.read_csv(csv_path, delimiter=';')
 
-print(df.head())
-print(df.keys())
+# Plot product midprice
 midprice = df[(df["product"] == PRODUCT)]["mid_price"]
-print(len(midprice))
+x = np.linspace(-SMOOTHING_PERIOD // 2, SMOOTHING_PERIOD // 2, SMOOTHING_PERIOD)
+smoothing_kernel = np.exp(-x ** 2 / (2 * SMOOTHING_SIGMA ** 2))
+smoothing_kernel /= smoothing_kernel.sum()
 
-midprice.plot()
-plt.savefig('plot.png')
+midprice_smooth = np.convolve(midprice.values, smoothing_kernel, mode="valid")
+
+plt.plot(midprice_smooth)
+plt.savefig('plots/plot.png')
 plt.show()
