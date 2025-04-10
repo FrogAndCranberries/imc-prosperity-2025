f3f96786d29033f0c10d3c86e8057f25dbf27cde
ttwardoch <ttwardoch5284@gmail.com>
2025-04-10 22:41:16 +0200

    Smoothing is optional

--- plot-prices.py
+++ plot-prices.py
@@ -3,24 +3,28 @@ import pandas as pd
 import numpy as np
 
 # Variables
-DAY = 0
-PRODUCT = "KELP"
+ROUND = 2
+DAY = 1
+PRODUCT = "SQUID_INK"
+SMOOTHING = False
 SMOOTHING_PERIOD = 100
 SMOOTHING_SIGMA = 20
 
 # Load dataframe
-csv_path = f"./round-1-island-data-bottle/prices_round_1_day_{DAY}.csv"
-
+csv_path = f"./data/round-{ROUND}-island-data-bottle/prices_round_{ROUND}_day_{DAY}.csv"
 df = pd.read_csv(csv_path, delimiter=';')
 
 # Plot product midprice
 midprice = df[(df["product"] == PRODUCT)]["mid_price"]
-x = np.linspace(-SMOOTHING_PERIOD // 2, SMOOTHING_PERIOD // 2, SMOOTHING_PERIOD)
-smoothing_kernel = np.exp(-x ** 2 / (2 * SMOOTHING_SIGMA ** 2))
-smoothing_kernel /= smoothing_kernel.sum()
 
-midprice_smooth = np.convolve(midprice.values, smoothing_kernel, mode="valid")
+if SMOOTHING:
+    x = np.linspace(-SMOOTHING_PERIOD // 2, SMOOTHING_PERIOD // 2, SMOOTHING_PERIOD)
+    smoothing_kernel = np.exp(-x ** 2 / (2 * SMOOTHING_SIGMA ** 2))
+    smoothing_kernel /= smoothing_kernel.sum()
 
-plt.plot(midprice_smooth)
-plt.savefig('plots/plot.png')
+    midprice = np.convolve(midprice.values, smoothing_kernel, mode="valid")
+
+plt.plot(midprice)
+plt.title(f"{PRODUCT}, ROUND {ROUND}, DAY {DAY}")
+plt.savefig(f"plots/{PRODUCT}_R{ROUND}_D{DAY}.png")
 plt.show()
