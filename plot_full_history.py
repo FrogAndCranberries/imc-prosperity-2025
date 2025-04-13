003e6aa42a1d3132956e9c415471284a7225803b
ttwardoch <ttwardoch5284@gmail.com>
2025-04-13 15:51:26 +0200

    Plots everything in one run

--- plot_full_history.py
+++ plot_full_history.py
@@ -1,23 +1,30 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from tqdm import tqdm
 
 from helper import get_midprice, get_full_midprice, midprice_smoothing
 
 # Variables
-PRODUCT = "PICNIC_BASKET2"
+PRODUCTS = ["KELP", "SQUID_INK", "RAINFOREST_RESIN",
+            "JAMS", "CROISSANTS", "DJEMBES", "PICNIC_BASKET2", "PICNIC_BASKET2",
+            "VOLCANIC_ROCK", "VOLCANIC_ROCK_VOUCHER_9500", "VOLCANIC_ROCK_VOUCHER_9750", "VOLCANIC_ROCK_VOUCHER_10000", "VOLCANIC_ROCK_VOUCHER_10250", "VOLCANIC_ROCK_VOUCHER_10500"]
 
 SMOOTHING = False
 SMOOTHING_PERIOD = 120
 SMOOTHING_SIGMA = 20
 
-midprice_long = get_full_midprice(PRODUCT)
+for PRODUCT in tqdm(PRODUCTS):
+    midprice_long = get_full_midprice(PRODUCT)
 
-if SMOOTHING:
-    midprice_long = midprice_smoothing(midprice_long, SMOOTHING_PERIOD, SMOOTHING_SIGMA)
+    if SMOOTHING:
+        midprice_long = midprice_smoothing(midprice_long, SMOOTHING_PERIOD, SMOOTHING_SIGMA)
 
-plt.plot(midprice_long)
+    plt.plot(midprice_long)
 
-plt.title(f"FULL HISTORY OF {PRODUCT}")
-plt.savefig(f"plots/MULTIDAY_{PRODUCT}.png")
+    plt.title(f"FULL HISTORY OF {PRODUCT}")
+    plt.savefig(f"plots/MULTIDAY_{PRODUCT}.png")
+    plt.show(block=False)
+    plt.clf()
+
 plt.show()
