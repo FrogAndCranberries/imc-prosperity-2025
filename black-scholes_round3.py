0c7d84fd5455afff921ad42d28d6e2a2cdf3804e
Kordian-G <kordian.glabowski@gmail.com>
2025-04-15 23:35:55 +0200

    added data 3 and set up BS strategy

---
+++ black-scholes_round3.py
@@ -0,0 +1,6 @@
+from datamodel import OrderDepth, UserId, TradingState, Order, ConversionObservation
+from typing import List, Dict, Any
+import string
+import jsonpickle
+import numpy as np
+import math
