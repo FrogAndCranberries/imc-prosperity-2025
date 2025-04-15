7230c4b0290bf85c42f2cd672c596b4cec7725da
Kordian-G <kordian.glabowski@gmail.com>
2025-04-16 00:17:28 +0200

    in case

--- black-scholes_round3.py
+++ black-scholes_round3.py
@@ -4,3 +4,196 @@ import string
 import jsonpickle
 import numpy as np
 import math
+
+
+class Product:
+    VOLCANIC_ROCK = "VOLCANIC_ROCK"
+    VOLCANIC_ROCK_VOUCHER_9500 = "VOLCANIC_ROCK_VOUCHER_9500"
+    VOLCANIC_ROCK_VOUCHER_9750 = "VOLCANIC_ROCK_VOUCHER_9750"
+    VOLCANIC_ROCK_VOUCHER_10000 = "VOLCANIC_ROCK_VOUCHER_10000"
+    VOLCANIC_ROCK_VOUCHER_10250 = "VOLCANIC_ROCK_VOUCHER_10250"
+    VOLCANIC_ROCK_VOUCHER_10500 = "VOLCANIC_ROCK_VOUCHER_10500"
+    SPREAD = "SPREAD"
+
+
+PARAMS = {
+    
+    Product.VOLCANIC_ROCK: {
+    },
+    Product.SPREAD: {
+        "default_spread_mean": 379.50439988484239,
+        "default_spread_std": 76.07966,
+        "spread_std_window": 45,
+        "zscore_threshold": 7,
+        "target_position": 58,
+    },
+    Product.VOLCANIC_ROCK_VOUCHER_9500: {
+        "mean_volatility": 0.15959997370608378,
+        "threshold": 0.00163,
+        "strike": 9500,
+        "starting_time_to_expiry": 7,
+        "std_window": 6,
+        "zscore_threshold": 21,
+    },
+    Product.VOLCANIC_ROCK_VOUCHER_9750: {
+        "mean_volatility": 0.15959997370608378,
+        "threshold": 0.00163,
+        "strike": 9750,
+        "starting_time_to_expiry": 7,
+        "std_window": 6,
+        "zscore_threshold": 21,
+    },
+    Product.VOLCANIC_ROCK_VOUCHER_10000: {
+        "mean_volatility": 0.15959997370608378,
+        "threshold": 0.00163,
+        "strike": 10000,
+        "starting_time_to_expiry": 7,
+        "std_window": 6,
+        "zscore_threshold": 21,
+    },
+    Product.VOLCANIC_ROCK_VOUCHER_10250: {
+        "mean_volatility": 0.15959997370608378,
+        "threshold": 0.00163,
+        "strike": 10250,
+        "starting_time_to_expiry": 7,
+        "std_window": 6,
+        "zscore_threshold": 21,
+    },
+    Product.VOLCANIC_ROCK_VOUCHER_10500: {
+        "mean_volatility": 0.15959997370608378,
+        "threshold": 0.00163,
+        "strike": 10500,
+        "starting_time_to_expiry": 7,
+        "std_window": 6,
+        "zscore_threshold": 21,
+    },
+
+}
+
+from math import log, sqrt, exp
+from statistics import NormalDist
+
+
+class BlackScholes:
+    @staticmethod
+    def black_scholes_call(spot, strike, time_to_expiry, volatility):
+        d1 = (
+            log(spot) - log(strike) + (0.5 * volatility * volatility) * time_to_expiry
+        ) / (volatility * sqrt(time_to_expiry))
+        d2 = d1 - volatility * sqrt(time_to_expiry)
+        call_price = spot * NormalDist().cdf(d1) - strike * NormalDist().cdf(d2)
+        return call_price
+
+    @staticmethod
+    def black_scholes_put(spot, strike, time_to_expiry, volatility):
+        d1 = (log(spot / strike) + (0.5 * volatility * volatility) * time_to_expiry) / (
+            volatility * sqrt(time_to_expiry)
+        )
+        d2 = d1 - volatility * sqrt(time_to_expiry)
+        put_price = strike * NormalDist().cdf(-d2) - spot * NormalDist().cdf(-d1)
+        return put_price
+
+    @staticmethod
+    def delta(spot, strike, time_to_expiry, volatility):
+        d1 = (
+            log(spot) - log(strike) + (0.5 * volatility * volatility) * time_to_expiry
+        ) / (volatility * sqrt(time_to_expiry))
+        return NormalDist().cdf(d1)
+
+    @staticmethod
+    def gamma(spot, strike, time_to_expiry, volatility):
+        d1 = (
+            log(spot) - log(strike) + (0.5 * volatility * volatility) * time_to_expiry
+        ) / (volatility * sqrt(time_to_expiry))
+        return NormalDist().pdf(d1) / (spot * volatility * sqrt(time_to_expiry))
+
+    @staticmethod
+    def vega(spot, strike, time_to_expiry, volatility):
+        d1 = (
+            log(spot) - log(strike) + (0.5 * volatility * volatility) * time_to_expiry
+        ) / (volatility * sqrt(time_to_expiry))
+        # print(f"d1: {d1}")
+        # print(f"vol: {volatility}")
+        # print(f"spot: {spot}")
+        # print(f"strike: {strike}")
+        # print(f"time: {time_to_expiry}")
+        return NormalDist().pdf(d1) * (spot * sqrt(time_to_expiry)) / 100
+
+    @staticmethod
+    def implied_volatility(
+        call_price, spot, strike, time_to_expiry, max_iterations=200, tolerance=1e-10
+    ):
+        low_vol = 0.01
+        high_vol = 1.0
+        volatility = (low_vol + high_vol) / 2.0  # Initial guess as the midpoint
+        for _ in range(max_iterations):
+            estimated_price = BlackScholes.black_scholes_call(
+                spot, strike, time_to_expiry, volatility
+            )
+            diff = estimated_price - call_price
+            if abs(diff) < tolerance:
+                break
+            elif diff > 0:
+                high_vol = volatility
+            else:
+                low_vol = volatility
+            volatility = (low_vol + high_vol) / 2.0
+        return volatility
+
+class Trader:
+    def __init__(self, params=None):
+        if params is None:
+            params = PARAMS
+        self.params = params
+
+        self.LIMIT = {
+            Product.VOLCANIC_ROCK_VOUCHER_9500: 200,
+            Product.VOLCANIC_ROCK_VOUCHER_9750: 200,
+            Product.VOLCANIC_ROCK_VOUCHER_10000: 200,
+            Product.VOLCANIC_ROCK_VOUCHER_10250: 200,
+            Product.VOLCANIC_ROCK_VOUCHER_10500: 200,
+        }
+        # Returns buy_order_volume, sell_order_volume
+    def take_best_orders(
+        self,
+        product: str,
+        fair_value: int,
+        take_width: float,
+        orders: List[Order],
+        order_depth: OrderDepth,
+        position: int,
+        buy_order_volume: int,
+        sell_order_volume: int,
+        prevent_adverse: bool = False,
+        adverse_volume: int = 0,
+    ) -> (int, int):
+        position_limit = self.LIMIT[product]
+        if len(order_depth.sell_orders) != 0:
+            best_ask = min(order_depth.sell_orders.keys())
+            best_ask_amount = -1 * order_depth.sell_orders[best_ask]
+
+            if best_ask <= fair_value - take_width:
+                quantity = min(
+                    best_ask_amount, position_limit - position
+                )  # max amt to buy
+                if quantity > 0:
+                    orders.append(Order(product, best_ask, quantity))
+                    buy_order_volume += quantity
+                    order_depth.sell_orders[best_ask] += quantity
+                    if order_depth.sell_orders[best_ask] == 0:
+                        del order_depth.sell_orders[best_ask]
+
+        if len(order_depth.buy_orders) != 0:
+            best_bid = max(order_depth.buy_orders.keys())
+            best_bid_amount = order_depth.buy_orders[best_bid]
+            if best_bid >= fair_value + take_width:
+                quantity = min(
+                    best_bid_amount, position_limit + position
+                )  # should be the max we can sell
+                if quantity > 0:
+                    orders.append(Order(product, best_bid, -1 * quantity))
+                    sell_order_volume += quantity
+                    order_depth.buy_orders[best_bid] -= quantity
+                    if order_depth.buy_orders[best_bid] == 0:
+                        del order_depth.buy_orders[best_bid]
+        return buy_order_volume, sell_order_volume
