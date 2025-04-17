1a5ece99662aa9ee0ef165e13fa680e4df49468b
ttwardoch <ttwardoch5284@gmail.com>
2025-04-17 20:12:58 +0200

    Containers.py used to choose suitcases

--- containers.py
+++ containers.py
@@ -2,11 +2,11 @@ import numpy as np
 from tqdm import tqdm
 
 # Container data
-multipliers = np.array([10, 17, 20, 31, 37, 50, 73, 80, 89, 90], dtype=float)
-inhabitants = np.array([1, 1, 2, 2, 3, 4, 4, 6, 8, 10], dtype=float)
+multipliers = np.array([10, 17, 20, 23, 30, 31, 37, 40, 41, 47, 50, 60, 70, 73, 79, 80, 83, 89, 90, 100], dtype=float)
+inhabitants = np.array([1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 6, 7, 8, 10, 15], dtype=float)
 
 # Initialize number of picks per container
-picks = np.zeros(10)
+picks = np.zeros(20) + 0.0000000001
 
 for num_choices in tqdm(range(1, 100000)):  # simulate until convergence
     # Compute value of each container based on current load
@@ -14,10 +14,11 @@ for num_choices in tqdm(range(1, 100000)):  # simulate until convergence
 
     # Assign choices greedily
     top = np.argsort(value)[-1]
-    picks[top] += 1
+    #picks[top] += 1
 
-print([float(f'%.5f' % x) for x in picks/np.sum(picks)])
+print([float(f'%.3f' % x) for x in multipliers])
+print([float(f'%.3f' % x) for x in picks/np.sum(picks)])
 
 value = multipliers / (inhabitants + picks / np.sum(picks) * 100)
 
-print([float(f'%.5f' % x) for x in value])
+print([float(f'%.3f' % x) for x in value])
